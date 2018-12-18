17844c348313bf718c46a924167d418cd03ccf79
TheDarthMole <DarthMole@protonmail.com>
2018-12-18 10:55:18 +0000

    Comments and module install function

--- Classes Server.py
+++ Classes Server.py
@@ -1,18 +1,30 @@
-
 # Importing modules
 import socket, sys, os, time, sqlite3, select, threading, pickle, binascii, base64, hashlib, traceback
 from random import randint
+
+def installModule(package):
+    import subprocess
+    import sys
+    try:
+        subprocess.call([sys.executable, "-m", "pip", "install", package])
+    except:
+        print("[!] Failed to install {}".format(package))
+        
+installModule("pycryptodome")
+installModule("requests")
+
 try:
     from Crypto import Random
     from Crypto.Cipher import AES
 except ImportError:
     print("[!] pyCrypto module not installed! Install using in cmd 'py -m pip install pyCrypto' ")
     exit(1)
-
 try:
     from requests import get # Non-standard library
 except ImportError:
-    print("[!] requests module not installed, however it is not necessary")
+    print("[!] requests module not installed, installing it now")
+    installModule()
+
 
 # Declare public variables and initialize
 
@@ -95,27 +107,43 @@ class SQLDatabase:
 
     def isAdmin(self, username):
         data = self.CommandDB("SELECT accountType FROM clients WHERE nickname = ?",username)
-        if str(data[0][0]) == "Admin":
+        # Queries database see if user is in database, then gets the "accountType"
+        if str(data[0][0]) == "Admin": # If if's an admin, return true
             return True
-        return False
+        return False # If the previous statement is false then this is run
 
     def PrintCustomerContents(self): # Complete
         data = self.CommandDB("SELECT * FROM clients")
+        # Data is all of the enteries from the database in a 2d array
         print("\n{:^18} | {:^10} | {:^18} | {:^10} | {:^10}\n".format("IP","PORT","NickName","AccType","Password")+"-"*132)
+        # Formatting for display purposes
         for row in data:
             print("{:^18} | {:^10} | {:^18} | {:^10} | {:^10}".format(row[0],row[1],row[2],row[4], row[3]))
+        # Loops through the data and prints it in a nice format
+        # This means that no thrid party software is needed
+        # So it can be run on a terminal easily.
         print()
 
     def checkPassword(self, username, password):
         data = self.CommandDB("SELECT password FROM clients WHERE nickname = ?", username)
+        # Returns the password from the database with the selected user
         if len(data) == 1 and data[0][0] == password:
             return True
+        # Returns true if the passwords are equvivlent
+        # Else it returns false
         return False
 
     def AppendClientsDatabase(self, ip, port, nickname, password, accountType): # Complete
         self.CommandDB("INSERT INTO clients VALUES (?,?,?,?,?)",ip, port, nickname, password, accountType)
+        # Adds a client to the database with passed in data
 
     def updateUser(self,User,**kwargs):
+        # kwargs can be used to update the users table
+        # Using the key as the database field name and
+        # The data to update the existing data
+        # As there are many possible enteries for kwargs
+        # Checks are done to make sure they correspond to
+        # Field names
         for x in ("password","ip","port","accountType","nickname"):
             if x in kwargs:
                 if x == "password":
@@ -125,13 +153,21 @@ class SQLDatabase:
     def allowedCreateAccount(self, ip, port, username): # Checks to see if the users ip and port are already in the database
         if self.CommandDB("SELECT * FROM clients WHERE ip = ? AND port = ?",ip, port):
             return False
+        # Returns false if there is already a users ip is already in the database
         if self.CommandDB("SELECT * FROM clients WHERE nickname = ?",username):
             return False
+        # Returns false if there is already an existing user with that name in the database
         return True
+        # Returns true if there are no conflictions
 
     def CreateClientsTable(self):
         try: # Used because the database may already exist
-            self.CommandDB("CREATE TABLE clients (ip text, port integer, nickname text, password text, accountType text, PRIMARY KEY (nickname))")
+            self.CommandDB("CREATE TABLE clients (ip text NOT NULL,\
+                            port integer NOT NULL,\
+                            nickname text NOT NULL,\
+                            password text NOT NULL,\
+                            accountType text NOT NULL,\
+                            PRIMARY KEY (nickname))")
             # Primary key is username so the name is unique
             # Ip is text, not number, as it includes "."
             # Password is text as it will contain hex hashed password
@@ -157,43 +193,58 @@ class SQLDatabase:
 
     def PrintBlockedContents(self): # Prints all of blockedUsers table with headders
         data = self.CommandDB("SELECT * FROM blockedUsers")
+        # Gets all data from blockedUsers tale in a 2d array format
         print("\n{:^16} | {:^16} | {:^7}\n".format("RelatingUser","RelationalUser","Type")+"-"*59)
+        # Displays collumn headders in a readable format
         for row in data:
             print("{:^16} | {:^16} | {:^7}".format(row[0],row[1],row[2]))
+        # Displays the data in different formatted rows
         print()
 
     def EditBlockedDatabase(self, Relating, Relational, Type):
         if Relating == Relational:
             print("[!] {} tried blocking themself".format(Relating))
             return False
+        # Checks if the user is trying to block themselves
         for x in (Relating, Relational):
             if not self.CommandDB("SELECT nickname FROM clients WHERE nickname = ?",x):
                 print("[=] {} tried blocking {} who is not in the database".format(Relating,x))
                 return False # Returns false becasue the client is not in the table
-
+        # If either users are not in the database then return false
         self.CommandDB("INSERT OR REPLACE INTO blockedUsers (relatingUser, relationalUser,type) VALUES (?,?,?)",Relating, Relational,Type)
+        # Inserts or updates the existing record to contain the correct data
         self.PrintCustomerContents()
+        # Displays the contents of the blocked table
+        # (This is not necessary however the server cant call this command from the command line)
         return True
+        # If checks succeed then return True
 
     def isBlocked(self, Relating, Relational,Type="Blocked"):
         data = self.CommandDB("SELECT * FROM blockedUsers WHERE relatingUser = ? AND relationalUser = ? AND type = ?", Relating, Relational, Type)
+        # Selects the data from the database where an exact criteria is met
         return True if data else False
+        # If there is an existing entry in the database then return true, else return false
 
     def currentlyBlockedUsers(self, Relating, Type="Blocked"):
         data = self.CommandDB("SELECT relationalUser FROM blockedUsers WHERE relatingUser = ? AND type = ?",Relating, Type)
+        #
         sterilizedOutput = []
         for x in data:
             sterilizedOutput.append(x[0])
         return sterilizedOutput
 
     def CreateMessageTable(self):
-        try:
+        try: # Used because the database may already exist
             self.CommandDB("CREATE TABLE messages (username text NOT NULL,\
                             message text NOT NULL,\
                             timedate text NOT NULL,\
                             FOREIGN KEY (username) REFERENCES clients(nickname))")
+            # Creates the table with foreign key as the username of the client
+            # Timedate is text as it needs to contain more information than timedate type
+            # Message contains the message that the user sent, including metadata
+            # Validation makes sure that the data cannot be empty
             print("[+] Messages Database successfully created")
-        except sqlite3.OperationalError:
+        except sqlite3.OperationalError: # Catches an exact error
             print("[=] Messages Database already created")
 
     def AddMessage(self, user, message):
